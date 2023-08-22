bfd477eaee61702906e2ad1def38fb6abcb6a7a9
Alisson da Silva Pereira <alisson.silva.pereira@gmail.com>
2023-08-22 20:36:53 -0300

    2

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+const PlaceholderImage = require('./assets/images/pokemon-detective-pikachu.gif');
+/*
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}*/
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' , fontSize:42 }} >Open up App.js to start working on your app!</Text>
+      <Image source={PlaceholderImage} style={styles.image} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#045',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 420,
+    height: 600,
+    borderRadius: 18,
   },
 });
